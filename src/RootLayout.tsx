d8990146db04792c580cffe62d8869314c63bc60
trivedidharmik <dmtrivedi2@gmail.com>
2025-03-13 20:18:06 -0300

    Implemented web analytics

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -2,11 +2,12 @@ import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import MadeWith from "./Components/MadeWith";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout() {
   return (
     <Box h="100vh" w="100%">
-        <NavBar />
+      <NavBar />
       <Outlet />
       <MadeWith />
     </Box>
